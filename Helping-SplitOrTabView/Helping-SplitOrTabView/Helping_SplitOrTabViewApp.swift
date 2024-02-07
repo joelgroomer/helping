b92ee7996fcbb657864a117f9164d45f7aad2319
joelgroomer <joelgroomer@users.noreply.github.com>
2024-02-06 18:27:29 -0800

    Project set up to basically match question's description

---
+++ Helping-SplitOrTabView/Helping-SplitOrTabView/Helping_SplitOrTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helping_SplitOrTabViewApp.swift
+//  Helping-SplitOrTabView
+//
+//  Created by Joel Groomer on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Helping_SplitOrTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
